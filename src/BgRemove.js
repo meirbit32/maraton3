703f7fd4d61d57393acda4a97fd33d9e52987fa5
meir biton <meirbit32@gmail.com>
2023-09-06 21:11:46 +0300

    lesson3

--- src/BgRemove.js
+++ src/BgRemove.js
@@ -2,9 +2,15 @@ import './BgRemove.css';
 import close1 from'./assets/close1.png';
 import logo from'./assets/logo.png';
 import banner from'./assets/banner.png';
-import DownloadImg from './DownloadImg'
+import DownloadImg from './DownloadImg';
+import {useState} from 'react';
 
 function BgRemove() {
+  const [tabname, settabname] = useState('no_bg');
+  function tab_click() {
+    settabname('original');
+}
+
   return (
     <div >
       <div className='bg_div_cont'>
@@ -18,7 +24,7 @@ function BgRemove() {
         <div className='main_cont'>
             <div className='main_left'>
                 <div className='middle_div_left'>
-                    <div className='tab_button_no_bg'> הוסר רקע </div>
+                    <div className='tab_button_no_bg' onClick={tab_click}> הוסר רקע </div>
                     <div className='tab_button_original'> מקורי </div>
                 </div>
                 <div className='main_left_footer'>
@@ -29,11 +35,26 @@ function BgRemove() {
             </div>
             <div className='main_right'>
                 <div className='middle_div_right'>
-                  <DownloadImg/>
+                  <DownloadImg 
+                  title="תמונה חינם"
+                  subtitle=" 612x408 תצוגה מקדימה של תמונה   "
+                  btnText="הורד"
+                  subsubtext="איכות טובה עד 0.25 מגה פיקסל" 
+                  borderFlag={true}
+                  newImage={false}
+                  />
+                  <DownloadImg
+                  title="pro"
+                  subtitle="1280x1920 תמונה מלאה "
+                  btnText=" HD הורד"
+                  subsubtext="איכות טובה עד 0.25 מגה פיקסל"
+                  borderFlag={false}
+                  newImage={true}
+                  />
                 </div>
             </div>
           </div>
-            <div classname="footer_cont">
+            <div className="footer_cont">
               <img src={logo} className='logo'/>
               <a href='http://www.google.com' target="_blank"><img src={banner} className='banner'/></a>
             </div>
--- src/DownloadImg.js
+++ src/DownloadImg.js
@@ -1,10 +1,17 @@
 import './DownloadImg.css'
+import { useState } from 'react';
+import newImage from './assets/new.png'
 
-
-function DownloadImg() {
+function DownloadImg(props) {
+  const [border, setborder] = useState(props.borderFlag);
   return (
-        <div>
-            jkdhgf
+        <div className="DownloadImg_cont" style={{borderBottom : ( border?  "1px solid #9e9e9ea8" : "")}}>
+            <div className="DownloadImg_title" > {props.title} 
+            {props.newImage ? <img className="newImg" src={newImage} /> : ""}
+            </div>
+            <div className="DownloadImg_subtitle"> {props.subtitle}</div>
+            <button className="DownloadImg_btn">{props.btnText}</button>
+            <div className="DownloadImg_subsubtext">{props.subsubtext}</div>
         </div>
   )}
 
