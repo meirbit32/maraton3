ccd295927c433386ec0b8a686c4930458c97e329
meir biton <meirbit32@gmail.com>
2023-09-06 19:03:02 +0300

    add DownloadImg component

--- src/BgRemove.js
+++ src/BgRemove.js
@@ -1,6 +1,8 @@
 import './BgRemove.css';
-import close1 from'./assets/close1.png'
-
+import close1 from'./assets/close1.png';
+import logo from'./assets/logo.png';
+import banner from'./assets/banner.png';
+import DownloadImg from './DownloadImg'
 
 function BgRemove() {
   return (
@@ -11,6 +13,29 @@ function BgRemove() {
             <div className='bg_div_header_title'> העלאת תמונה כדי להסיר את הרקע</div>
             <button className='bg_div_header_button'> העלאת תמונה</button>
             <div className='bg_div_header_subtext'> פורמטים נתמכים png, jpeg</div>
+            </div>
+
+        <div className='main_cont'>
+            <div className='main_left'>
+                <div className='middle_div_left'>
+                    <div className='tab_button_no_bg'> הוסר רקע </div>
+                    <div className='tab_button_original'> מקורי </div>
+                </div>
+                <div className='main_left_footer'>
+                    <div className='main_left_footer_text'> על ידי העלאת תמונה אתה מסכים לתנאים וההגבלות . </div>
+                    <button className='main_left_footer_button'> תקנון חברה</button>
+                </div>
+
+            </div>
+            <div className='main_right'>
+                <div className='middle_div_right'>
+                  <DownloadImg/>
+                </div>
+            </div>
+          </div>
+            <div classname="footer_cont">
+              <img src={logo} className='logo'/>
+              <a href='http://www.google.com' target="_blank"><img src={banner} className='banner'/></a>
             </div>
       </div>
     </div>
---
+++ src/DownloadImg.js
@@ -0,0 +1,11 @@
+import './DownloadImg.css'
+
+
+function DownloadImg() {
+  return (
+        <div>
+            jkdhgf
+        </div>
+  )}
+
+  export default DownloadImg
