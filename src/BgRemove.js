d804d34291ff8452803a6f70d3bdfdd7ec84da40
meir biton <meirbit32@gmail.com>
2023-09-08 16:21:38 +0300

    lesson 5 half

--- src/BgRemove.js
+++ src/BgRemove.js
@@ -1,5 +1,5 @@
 import './BgRemove.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import DownloadIm from './DownloadIm';
 import Image from './Image'
 import logo from './assets/logo.png'
@@ -9,13 +9,30 @@ import close from './assets/close.png'
 function BgRemove() {
   
   const [tabname, settabname] = useState('no_bg');
+  const [open_poup, setopen_popup] = useState(false);
+
+  const inputFileElement = useRef()
+
+  const focusInput = () =>{
+    inputFileElement.current.click()
+  }
+
   function tab_click(e) {
-    
+
     if (e.target.className == "tab_button_original"){
       settabname('original')
     } else{
     settabname('no_bg');
 }}
+
+
+function open_popup (){
+  setopen_popup(true)
+}
+
+function close_popup(){
+  setopen_popup(false)
+}
 
   return (
     <div >
@@ -23,7 +40,8 @@ function BgRemove() {
           <div className='bg_div_header'>
             <img src={close} className='close1_img'/>
             <div className='bg_div_header_title'> העלאת תמונה כדי להסיר את הרקע</div>
-            <button className='bg_div_header_button'> העלאת תמונה</button>
+            <button className='bg_div_header_button' onClick={focusInput}> העלאת תמונה</button>
+            <input type='file' ref={inputFileElement} className='file_input'></input>
             <div className='bg_div_header_subtext'> פורמטים נתמכים png, jpeg</div>
             </div>
 
@@ -42,7 +60,7 @@ function BgRemove() {
                 </div>
                 <div className='main_left_footer'>
                     <div className='main_left_footer_text'> על ידי העלאת תמונה אתה מסכים לתנאים וההגבלות . </div>
-                    <button className='main_left_footer_button'> תקנון חברה</button>
+                    <button className='main_left_footer_button' onClick={open_popup}> תקנון חברה</button>
                 </div>
                 
 
@@ -73,6 +91,19 @@ function BgRemove() {
               <a href='http://www.google.com' target="_blank"><img src={banner} className='banner'/></a>
             </div>
       </div>
+      {open_poup ? 
+      <>
+        <div className='takanon_popup_overlay'></div>
+        <div className='takanon_popup'>
+          <img src={close}  className='close_popup_img' onClick={close_popup}/>
+        What is Lorem Ipsum?
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+          into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+           passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </div></>
+    : "" }
+
     </div>
   );
 }
--- src/Image.js
+++ src/Image.js
@@ -1,20 +1,25 @@
 import './Image.css'
-import { useState, useRef} from 'react'
+import { useState, useRef,useEffect} from 'react'
 
 
 function Image(props) {
     
     const [color, setcolor] = useState('red');
+    const [image_only_disp, setimage_only_disp] = useState('');
     
-       
-    
+    useEffect(() =>{
+        if(props.image_only == true){
+            setimage_only_disp('image_only_disp')
+        } else
+        setimage_only_disp('')
+    },[props.image_only]);
     const inputElement =useRef();
     
     const focusInput = ()=>{
         inputElement.current.click();
     };
 
-    function coor_change (e){
+    function color_change (e){
         setcolor(e.target.value)
     }
 
@@ -27,10 +32,10 @@ function Image(props) {
                 <div className='color_btn' onClick={focusInput}> צבע רקע 
                 <span className='color_choos' style={{backgroundColor : color}}></span>
                 </div>
-                <input type='color' ref={inputElement} className='input_color' onChange={coor_change}/>
+                <input type='color' ref={inputElement} className='input_color' onChange={color_change}/>
                 </>
                 : ''}
-                <div className='image_place'>
+                <div className={`image_place ${image_only_disp}`}>
 
                 </div>
 
